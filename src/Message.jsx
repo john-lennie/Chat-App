b422ec687f80399399460861c2cdef2b71d7a411
John Lennie <johnanthonylennie@gmail.com>
2019-07-24 23:44:48 -0400

    converted Message and MessageList to functional components

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,21 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Message extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <div className="message">
-          <span className="message-username">{this.props.user}</span>
-          <span className="message-content">{this.props.content}</span>
-        </div>
-        {/* <div className="message system">
-          Anonymous1 changed their name to nomnom.
-        </div> */}
+function Message(props) {
+  return (
+    <div>
+      <div className="message">
+        <span className="message-username">{props.user}</span>
+        <span className="message-content">{props.content}</span>
       </div>
-    );
-  }
+      {/* <div className="message system">
+        Anonymous1 changed their name to nomnom.
+      </div> */}
+    </div>
+  );
 }
 export default Message;
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,18 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Message from './Message.jsx';
 
-class MessageList extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const messages = this.props.messages.map((message, index) =>
+function MessageList(props) {
+    const messages = props.messages.map((message, index) => 
       // Only do this if items have no stable IDs
       <Message key={index} user={message.username} content={message.content} />
     );
-    return (
-        <div>{messages}</div>
-    )
-  }
+    return <div>{messages}</div>;
 }
+
 export default MessageList;
